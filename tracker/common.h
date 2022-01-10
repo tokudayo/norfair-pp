ade5a0bd4d04ecae9a521155ad31dff3560085a3
20toduc01 <20toduc01@gmail.com>
2022-01-10 22:11:10 +0700

    Another implementation checkpoint

--- tracker/common.h
+++ tracker/common.h
@@ -11,6 +11,12 @@ typedef Matrix<FLOAT_T, 1, 2> Point;
 typedef std::vector<Point> PointArray;
 typedef Matrix<FLOAT_T, Dynamic, 2> PointMatrix;
 
+struct Detection
+{
+    Point point;
+    int ID;
+};
+
 inline PointMatrix PointArrayToMatrix(PointArray& src) {
     PointMatrix dst(src.size());
     for (size_t i = 0; i < src.size(); i++) {
--- tracker/tracker.cpp
+++ tracker/tracker.cpp
@@ -1,7 +1,10 @@
 #include <vector>
+#include <unordered_set>
 #include <iostream>
 #include "common.h"
 #include <tracked_object.h>
+
+typedef std::vector<TrackedObject> TrackedObjArray;
 
 class Tracker 
 {
@@ -14,7 +17,8 @@ public:
             int point_transience = -1) 
     {
         std::cout << "Tracker()" << std::endl;
-        this->tracked_objects = new std::vector<TrackedObject>();
+        // this->tracked_objects = new std::vector<TrackedObject>();
+        this->tracked_objects = TrackedObjArray();
         this->hit_inertia_max = hit_inertia_max;
         this->hit_inertia_min = hit_inertia_min;
         this->nextID = 0;
@@ -39,7 +43,7 @@ public:
         this->period = period;
 
         if (detections != nullptr) 
-        {    
+        {
         }
         std::vector<TrackedObject> a;
         // Update tracked object list
@@ -56,17 +60,17 @@ public:
         {
             obj.tracker_step();
         }
-        std::vector<TrackedObject> initializing_objs;
-        std::vector<TrackedObject> initialized_objs;
+        std::vector<TrackedObject*> initializing_objs;
+        std::vector<TrackedObject*> initialized_objs;
 
         for (auto obj : tracked_objects) 
         {
             if (obj.is_initializing()) 
             {
-                initialized_objs.push_back(obj);
+                initialized_objs.push_back(&obj);
             } else 
             {
-                initializing_objs.push_back(obj);
+                initializing_objs.push_back(&obj);
             }
         }
 
@@ -76,10 +80,85 @@ public:
 
     }
 
+    std::tuple<std::vector<std::pair<int, int>>, std::vector<int>> 
+    UpdateObjectInPlace(
+        const std::vector<TrackedObject*> &objects,
+        const std::vector<Detection> &detections) 
+    {
+        int num_dets = detections.size();
+        int num_objs = objects.size();
+        if (num_dets == 0) 
+        {
+            return std::make_tuple(std::vector<std::pair<int, int>>(), std::vector<int>());
+        }
+        if (num_objs == 0)
+        {
+            std::vector<int> unmatched_dets = std::vector<int>();
+            for (int i = 0; i < num_dets; i++) 
+            {
+                unmatched_dets.push_back(i);
+            }
+            return std::make_tuple(std::vector<std::pair<int, int>>(), unmatched_dets);
+        }
+
+        // Calculate distance between detections and objects
+        MatrixXf dist_matrix = MatrixXf::Zero(detections.size(), objects.size());
+        for (int i = 0; i < detections.size(); i++) {
+            for (int j = 0; j < objects.size(); j++) {
+                dist_matrix(i, j) = (detections[i].point - objects[j]->estimate()).norm();
+            }
+        };        
+
+        // Need to merge it with the above part later now im too lazy to do it
+        
+        // To keep track of matched pairs
+        std::vector<std::pair<int,int>> det_obj_pairs;
+        std::vector<int> unmatched_dets;
+        std::unordered_set<int> matched_dets, matched_objs = {};
+        std::vector< std::pair<int, float> > dist_flattened;
+        
+
+        // Flatten the distance matrix and sort by distance in ascending order
+        // In the future, maybe pass in a long vector instead of a matrix
+        for (int i = 0; i < num_dets; i++) {
+            for (int j = 0; j < num_objs; j++) {
+                dist_flattened.push_back(std::make_pair(i*num_objs + j, dist_matrix(i, j)));
+            }
+        }
+        std::sort(dist_flattened.begin(), dist_flattened.end(), [](
+            const std::pair<int, float> &a,
+            const std::pair<int, float> &b) {
+                return a.second < b.second;
+            });
+
+        // Matching
+        for (int i = 0; i < dist_flattened.size(); i++) {
+            if (dist_flattened[i].second > dist_threshold)
+                break;
+            int det_idx = dist_flattened[i].first / dist_matrix.cols();
+            int obj_idx = dist_flattened[i].first % dist_matrix.cols();
+            if (matched_dets.find(det_idx) == matched_dets.end()
+                && matched_objs.find(obj_idx) == matched_objs.end()) 
+            {
+                matched_dets.insert(det_idx);
+                matched_objs.insert(obj_idx);
+            }
+            det_obj_pairs.push_back(std::make_pair(det_idx, obj_idx));
+        }
+
+        // Find unmatched detections
+        for (int i = 0; i < num_dets; i++) {
+            if (matched_dets.find(i) == matched_dets.end()) {
+                unmatched_dets.push_back(i);
+            }
+        }
+        return {det_obj_pairs, unmatched_dets};
+    } 
+
 
 
 private:
-    std::vector<TrackedObject> tracked_objects;
+    TrackedObjArray tracked_objects;
     FLOAT_T dist_threshold;
     int hit_inertia_min;
     int hit_inertia_max;
@@ -88,11 +167,5 @@ private:
     int initial_hit_count;
     int period;
     int point_transience;
-    void UpdateObjectInPlace(std::vector<TrackedObject> objs, PointArray)
+    void UpdateObjectInPlace(std::vector<TrackedObject> objs, PointArray &detections);
 };
-
-
-PYBIND11_MODULE(testmodule, m) {
-    py::class_<Tracker>(m, "Tracker")
-        .def(py::init<>());
-}
