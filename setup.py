aec92cd603b6fdd64c1752311770c7076acbb5e9
20toduc01 <20toduc01@gmail.com>
2022-01-17 17:13:59 +0700

    Initial

---
+++ setup.py
@@ -0,0 +1,27 @@
+from pybind11.setup_helpers import Pybind11Extension, build_ext
+from setuptools import setup
+
+__version__ = "0.0.1"
+
+ext_modules = [
+    Pybind11Extension("norfair_cpp",
+        ["pybinding.cpp"],
+        # Example: passing in the version to the compiled code
+        define_macros = [('VERSION_INFO', __version__)],
+        ),
+]
+
+setup(
+    name="norfair_cpp",
+    version=__version__,
+    author="To Duc",
+    author_email="20toduc01@gmail.com",
+    url="https://github.com/20toduc01/norfair-cpp",
+    description="Multi-object tracker using Kalman filter. 1 (x, y) point per object.",
+    long_description="",
+    ext_modules=ext_modules,
+    extras_require={},
+    cmdclass={"build_ext": build_ext},
+    zip_safe=False,
+    python_requires=">=3.6",
+)
